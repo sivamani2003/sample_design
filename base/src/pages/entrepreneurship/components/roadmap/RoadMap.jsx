747bf274a53539ca824aacd987481ea2c8bb18f2
siva <sivamani_k@srmap.edu.in>
2024-06-07 22:11:52 +0530

    update

--- base/src/pages/entrepreneurship/components/roadmap/RoadMap.jsx
+++ base/src/pages/entrepreneurship/components/roadmap/RoadMap.jsx
@@ -5,7 +5,7 @@ import renderImg from "../../images/entrepreneur.svg";
 const RoadMap = () => {
   return (
     <>
-    <div className="mt-[5rem] text-center mt-[10rem] ml-9">
+    <div className="text-center mt-[10rem] ml-9">
     <h1 className="text-center text-3xl md:text-5xl mb-10 font-bold">How it <span className="text-pink">Works ?</span></h1>
       </div>
     <div className="image-container px-4">
