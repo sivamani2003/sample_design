e3c034dcb1d8448c72d7365e80b40135a0466726
siva <sivamani_k@srmap.edu.in>
2024-06-07 22:35:34 +0530

    update

--- base/src/layout/Prime.jsx
+++ base/src/layout/Prime.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Footer from '../pages/footer/Footer';
-import NavBar from '../pages/navBar/Navbar';
+import NavBar from '../pages/navbar/Navbar';
 
 const Prime = () => {
     return (
